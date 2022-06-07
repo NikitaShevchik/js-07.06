1ce7e0cbf02260336f224d851eb290ca55e444c4
NikitaShevchik <shevchik.nikita@gmail.com>
2022-06-07 14:56:09 +0300

    promises+

--- js/scrypt.js
+++ js/scrypt.js
@@ -17,35 +17,58 @@ console.log('Request data');
 //     }, 2000)
 // }, 2000)
 
-const prep = new Promise(function (resolve, reject) {
-    setTimeout(() => {
-        console.log('Preparing data...')
-        const backendData = {
-            server: 'aws',
-            port: 2000,
-            status: 'working'
-        }
-        resolve(backendData)
-    }, 2000);
-})
 
-prep.then(data => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            data.modified = true,
-                reject(data)
-            // console.log('Data received', backendData)
-        }, 2000)
+
+
+// const prep = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         console.log('Preparing data...')
+//         const backendData = {
+//             server: 'aws',
+//             port: 2000,
+//             status: 'working'
+//         }
+//         resolve(backendData)
+//     }, 2000);
+// })
+
+// prep.then(data => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             data.modified = true,
+//                 reject(data)
+//             // console.log('Data received', backendData)
+//         }, 2000)
+//     })
+//     // prep2.then(clientData => {
+//     //     console.log('Data recieved', clientData)
+//     // })
+// })
+//     .then(clientData => {
+//         clientData.modified = true
+//         return clientData
+//     }).then(data => {
+//         console.log('Modified', data)
+//     })
+//     .catch(err => console.log('Error:', err))
+//     .finally(() => { console.log('Finally') })
+
+
+const sleep = ms => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), ms)
     })
-    // prep2.then(clientData => {
-    //     console.log('Data recieved', clientData)
-    // })
-})
-    .then(clientData => {
-        clientData.modified = true
-        return clientData
-    }).then(data => {
-        console.log('Modified', data)
+}
+
+// sleep(2000).then(() => console.log('After 2 sec'));
+// sleep(3000).then(() => console.log('After 3 sec'));
+
+Promise.all([sleep(2000), sleep(5000)])
+    .then(() => {
+        console.log('All promises')
     })
-    .catch(err => console.log('Error:', err))
-    .finally(() => { console.log('Finally') })
+
+Promise.race([sleep(2000), sleep(5000)])
+    .then(() => {
+        console.log('Race promises')
+    })
