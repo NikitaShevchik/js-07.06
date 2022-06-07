34606be91424b5f47e84186d9ef469930e54d461
NikitaShevchik <shevchik.nikita@gmail.com>
2022-06-07 14:00:34 +0300

    promise

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,1 +1,51 @@
 "use strict"
+
+console.log('Request data');
+
+// setTimeout(() => {
+//     console.log('Preparing data...');
+
+//     const backendData = {
+//         server: 'aws',
+//         port: 2000,
+//         status: 'working'
+//     }
+
+//     setTimeout(() => {
+//         backendData.modified = true,
+//         console.log('Data received', backendData)
+//     }, 2000)
+// }, 2000)
+
+const prep = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        console.log('Preparing data...')
+        const backendData = {
+            server: 'aws',
+            port: 2000,
+            status: 'working'
+        }
+        resolve(backendData)
+    }, 2000);
+})
+
+prep.then(data => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            data.modified = true,
+                reject(data)
+            // console.log('Data received', backendData)
+        }, 2000)
+    })
+    // prep2.then(clientData => {
+    //     console.log('Data recieved', clientData)
+    // })
+})
+    .then(clientData => {
+        clientData.modified = true
+        return clientData
+    }).then(data => {
+        console.log('Modified', data)
+    })
+    .catch(err => console.log('Error:', err))
+    .finally(() => { console.log('Finally') })
