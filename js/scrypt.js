e91161ea1a9700131739f49a68fa5b94b26374b8
NikitaShevchik <shevchik.nikita@gmail.com>
2022-06-07 16:54:43 +0300

    learn+obj+extends+class

--- js/scrypt.js
+++ js/scrypt.js
@@ -140,51 +140,102 @@
 //     hasTail: true
 // }
 
-class Animal {
-    static type = 'ANIMAL'
+// class Animal {
+//     static type = 'ANIMAL'
 
-    constructor(options) {
-        this.name = options.name
-        this.age = options.age
-        this.hasTail = options.hasTail
-    }
+//     constructor(options) {
+//         this.name = options.name
+//         this.age = options.age
+//         this.hasTail = options.hasTail
+//     }
 
-    voice() {
-        console.log('I am Animal')
-    }
-}
+//     voice() {
+//         console.log('I am Animal')
+//     }
+// }
 
-// const animal = new Animal({
-//     name: 'Jhon',
-//     age: 3,
-//     hasTail: true
+// // const animal = new Animal({
+// //     name: 'Jhon',
+// //     age: 3,
+// //     hasTail: true
+// // })
+
+// class Dog extends Animal {
+//     static type = 'DOG'
+
+//     constructor(options) {
+//         super(options)
+//         this.color = options.color
+//     }
+
+//     voice() {
+//         super.voice()
+//         console.log('I am dog')
+//     }
+
+//     get ageInfo() {
+//         return this.age * 7
+//     }
+
+//     set ageInfo(newAge) {
+//         this.age = newAge
+//     }
+// }
+
+// const dog = new Dog({
+//     name: 'Graph',
+//     age: 5,
+//     hasTail: true,
+//     color: 'White'
 // })
 
-class Dog extends Animal {
-    static type = 'DOG'
 
-    constructor(options) {
-        super(options)
-        this.color = options.color
+class Component {
+    constructor(selector) {
+        this.$el = document.querySelector(selector)
     }
 
-    voice() {
-        super.voice()
-        console.log('I am dog')
+    hide() {
+        this.$el.style.display = 'none'
     }
 
-    get ageInfo() {
-        return this.age * 7
-    }
-
-    set ageInfo(newAge) {
-        this.age = newAge
+    show() {
+        this.$el.style.display = 'block'
     }
 }
 
-const dog = new Dog({
-    name: 'Graph',
-    age: 5,
-    hasTail: true,
-    color: 'White'
+class Box extends Component {
+    constructor(options) {
+        super(options.selector)
+
+        this.$el.style.width = this.$el.style.height = options.size + 'px'
+
+        this.$el.style.background = options.color
+    }
+}
+
+const box1 = new Box({
+    selector: '#box1',
+    size: 100,
+    color: 'red'
+})
+
+const box2 = new Box({
+    selector: '#box2',
+    size: 200,
+    color: 'blue'
+})
+
+class Circle extends Box {
+    constructor(options) {
+        super(options)
+
+        this.$el.style.borderRadius = '50%'
+    }
+}
+
+const c = new Circle({
+    selector: '#circle1',
+    size: 300,
+    color: 'green'
 })
